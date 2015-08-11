5dc255ada7338442ae2f9eecf34437bf1b78f90a
David Gloe <dcgloe@gmail.com>
2015-08-11 08:21:55 -0500

    Refactor tests into functions

--- src/tests/id3test.c
+++ src/tests/id3test.c
@@ -6,26 +6,27 @@
 #include <stdlib.h>
 #include "../id3v2.h"
 
-int main() {
-    uint8_t sync[4];
-    uint8_t *outsync;
-    size_t outlen;
-
-    // Check type sizes
+void check_type_sizes(void) {
     assert(sizeof(uint24_t) == 3);
     assert(sizeof(struct id3v2_header) == 10);
     assert(sizeof(struct id3v2_footer) == 10);
     assert(sizeof(struct id3v2_frame_header) == 10);
+}
 
-    // Check synchsafe functions
+void check_synchsafe(void) {
     assert(to_synchsafe(0x0FFFFFFF) == 0x7F7F7F7F);
     assert(from_synchsafe(0x7F7F7F7F) == 0x0FFFFFFF);
     assert(to_synchsafe(0) == 0);
     assert(from_synchsafe(0) == 0);
     assert(to_synchsafe(0x00204080) == 0x01010100);
     assert(from_synchsafe(0x01010100) == 0x00204080);
+}
 
-    // Check unsynchronize/resynchronize
+void check_synchronize(void) {
+    uint8_t sync[4];
+    uint8_t *outsync;
+    size_t outlen;
+
     sync[0] = 0xFF;
     sync[1] = 0x0F;
     sync[2] = 0x00;
@@ -60,6 +61,12 @@ int main() {
     assert(outlen == 3);
     assert(outsync[0] == 0x01 && outsync[1] == 0xFF && outsync[2] == 0x01);
     free(outsync);
+}
+
+int main() {
+    check_type_sizes();
+    check_synchsafe();
+    check_synchronize();
 
     printf("Passed!\n");
     return 0;
